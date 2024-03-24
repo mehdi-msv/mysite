ebb9b5c045dedbcc4441add72f825cfbaf8bddbf
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2024-03-25 02:06:21 +0330

    add Post table to admin

--- blog/models.py
+++ blog/models.py
@@ -12,3 +12,5 @@ class Post(models.Model):
     counted_views = models.IntegerField(default = 0)
     created_date = models.DateTimeField(auto_now_add = True)
     updated_date = models.DateTimeField(auto_now = True)
+    def __str__(self):
+        return f'{self.title} {self.id}'
