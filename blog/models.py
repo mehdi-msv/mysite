3165f35c4cc822c413d017c3abe49560aaf0ec07
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2024-03-24 03:53:52 +0330

    create table and columns
    creating tables and columns in app model file

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    #author = models.ForeignKey(models.ForeignKey)
+    #category = models.ForeignKey(models.ForeignKey)
+    #tag = models.ForeignKey(models.ForeignKey)
+    #image = models.ImageField()
+    status = models.BooleanField(default = False)
+    published_date = models.DateField(null = True)
+    counted_views = models.IntegerField(default = 0)
+    created_date = models.DateTimeField(auto_now_add = True)
+    updated_date = models.DateTimeField(auto_now = True)
