7d13011368606c648db9c5658bf93cd915736370
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2024-03-26 00:56:39 +0330

    Attach the text to the blog

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,6 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
-    fields = ('id','title')
-    list_display = ('title','status','created_date','counted_views')
+    list_display = ('id','title','status','created_date','counted_views')
     list_filter = ('status',)
     search_fields = ['title','content']
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import Post
 def blog_view(request):
-    return render(request,'blog/blog-home.html')
+    posts = Post.objects.filter(status = 1)
+    context = {'posts':posts}
+    return render(request,'blog/blog-home.html', context)
 def blog_single(request):
-    return render(request,'blog/blog-single.html')
+    posts = Post.objects.filter(status = 1)
+    context = {'posts':posts}
+    return render(request,'blog/blog-single.html',context)
