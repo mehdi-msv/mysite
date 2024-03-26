4aad5a2e0857d64410f30cac5bfd97b2724eb978
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2024-03-26 17:44:55 +0330

    changes
    add published_date to admin
    Filtered post by published date

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post
-from datetime import datetime
+from django.utils import timezone
 def blog_view(request):
-    posts = Post.objects.exclude(published_date= datetime.now())
+    posts = Post.objects.exclude(published_date__gt = timezone.now()).exclude(status = 0)
 #    posts = Post.objects.filter(status = 1)
     context = {'posts':posts}
     return render(request,'blog/blog-home.html', context)
 def blog_single(request,pid):
-    post = get_object_or_404(Post, pk=pid)
+    posts = Post.objects.exclude(published_date__gt = timezone.now()).exclude(status = 0)
+    post = get_object_or_404(posts, pk=pid)
     context = {'post':post}
     post.counted_views += 1
     post.save()
