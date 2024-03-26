31d39de2cf45ff6aebf2d1abd6c7eb99fdf59275
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2024-03-26 07:02:08 +0330

    single-page

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
+from datetime import datetime
 def blog_view(request):
-    posts = Post.objects.filter(status = 1)
+    posts = Post.objects.exclude(published_date= datetime.now())
+#    posts = Post.objects.filter(status = 1)
     context = {'posts':posts}
     return render(request,'blog/blog-home.html', context)
-def blog_single(request):
-    posts = Post.objects.filter(status = 1)
-    context = {'posts':posts}
+def blog_single(request,pid):
+    post = get_object_or_404(Post, pk=pid)
+    context = {'post':post}
+    post.counted_views += 1
+    post.save()
     return render(request,'blog/blog-single.html',context)
