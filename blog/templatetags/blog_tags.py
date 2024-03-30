9caceef091de626525ca6c4b8dd692fac83ca847
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2024-03-30 15:05:47 +0330

    category
    add dynamic categories to home blog

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,8 +1,16 @@
 from django import template
-from blog.models import Post
+from blog.models import *
 from django.utils import timezone
 register = template.Library()
 @register.inclusion_tag('blog/blog-popular.html')
 def blog_latest(args=3):
     posts = Post.objects.filter(status=1,published_date__lte = timezone.now()).order_by('-published_date')[:args]
     return {'posts':posts}
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    posts = Post.objects.filter(status = 1)
+    categories = Categories.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category = name).count()
+    return {'categories': cat_dict}
