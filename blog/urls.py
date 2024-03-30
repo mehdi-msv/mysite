db09a303bf9ba09dfd910c7665f392ff3298146b
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2024-03-31 01:37:12 +0330

    author
    connect two url to one view

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path('',blog_view,name='blog'),
     path('<int:pid>',blog_single,name='single'),
+    path('category/<str:cat_name>',blog_view,name='category'),
+    path('author/<str:author_user>',blog_view,name='author')
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Post
+from .models import *
 from django.utils import timezone
-def blog_view(request):
+def blog_view(request,**kwargs):
     #posts = Post.objects.filter(published_date__lte = timezone.now()).exclude(status = 0).order_by('-published_date')
-    posts = Post.objects.filter(published_date__lte = timezone.now(),status = 1,).order_by('-id')
+    posts = Post.objects.filter(published_date__lte = timezone.now(),status = 1,).order_by('-published_date')
+    if kwargs.get('cat_name'):
+        posts = posts.filter(category__name = kwargs['cat_name'])
+    if kwargs.get('author_user'):
+        posts = posts.filter(author__username = kwargs['author_user'])
     context = {'posts':posts,}
     return render(request,'blog/blog-home.html', context)
 def blog_single(request,pid):
@@ -15,3 +19,8 @@ def blog_single(request,pid):
     post.counted_views += 1
     post.save()
     return render(request,'blog/blog-single.html',context)
+def blog_category(request, cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts': posts}
+    return render(request,'blog/blog-home.html',context)
