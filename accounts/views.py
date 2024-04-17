227e531fc069b1fb306af22fda6d9239151c4eb2
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2024-04-17 16:54:47 +0330

    logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 def login_view(request):
     if request.user.is_authenticated:
         return redirect('/')
@@ -17,6 +18,10 @@ def login_view(request):
     form = AuthenticationForm()
     context = {'form': form}
     return render(request,'accounts/login.html',context)
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('/')
     
         
             
